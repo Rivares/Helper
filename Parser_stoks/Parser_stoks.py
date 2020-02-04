bd33d99baf59043b66390153d632d6de86897e24
Rivares <ura-nikolaevic@yandex.ru>
2020-02-05 00:41:22 +0300

    Implement: Momentum and Volume Indicators

--- Parser_stoks/Parser_stoks.py
+++ Parser_stoks/Parser_stoks.py
@@ -90,8 +90,8 @@ def main():
     # # __________________________________________ Keltner Channel __________________________________________
     #
     # # Initialize Keltner Channel Indicator
-    # indicator_kc = ta.volatility.KeltnerChannel(high=df["<HIGH>"], low=df["<LOW>"], close=df["<CLOSE>"], n=20,
-    #                                             fillna=True)
+    # indicator_kc = ta.volatility.KeltnerChannel(high=df["<HIGH>"],
+    #                                             low=df["<LOW>"], close=df["<CLOSE>"], n=20, fillna=True)
     #
     # # Add Keltner Channel features
     # df['kc_kcc'] = indicator_kc.keltner_channel_central()
@@ -115,7 +115,9 @@ def main():
     # # __________________________________________ Average true range (ATR) __________________________________________
     #
     # # Initialize Average true range Indicator
-    # indicator_atr = ta.volatility.AverageTrueRange(high=df["<HIGH>"], low=df["<LOW>"], close=df["<CLOSE>"], n=20, fillna=True)
+    # indicator_atr = ta.volatility.AverageTrueRange(high=df["<HIGH>"],
+    #                                                low=df["<LOW>"],
+    #                                                close=df["<CLOSE>"], n=20, fillna=True)
     #
     # # Add ATR indicator
     # df['atr_i'] = indicator_atr.average_true_range()
@@ -154,7 +156,9 @@ def main():
     # # _____________________________ Average Directional Movement Index (ADX) ________________________________
     #
     # # Initialize ADX Indicator
-    # indicator_adx = ta.trend.ADXIndicator(high=df["<HIGH>"], low=df["<LOW>"], close=df["<CLOSE>"], n=20, fillna=True)
+    # indicator_adx = ta.trend.ADXIndicator(high=df["<HIGH>"],
+    #                                       low=df["<LOW>"],
+    #                                       close=df["<CLOSE>"], n=20, fillna=True)
     #
     # # Add ADX features
     # df['adx_aver'] = indicator_adx.adx()
@@ -190,7 +194,9 @@ def main():
     # # _____________________________ Commodity Channel Index (CCI) ________________________________
     #
     # # Initialize ADX Indicator
-    # indicator_ccl = ta.trend.CCIIndicator(high=df["<HIGH>"], low=df["<LOW>"], close=df["<CLOSE>"], n=20, c=5, fillna=True)
+    # indicator_ccl = ta.trend.CCIIndicator(high=df["<HIGH>"],
+    #                                       low=df["<LOW>"],
+    #                                       close=df["<CLOSE>"], n=20, c=5, fillna=True)
     #
     # # Add ADX features
     # df['ccl_i'] = indicator_ccl.cci()
@@ -231,7 +237,8 @@ def main():
     # # _____________________________ Ichimoku Kinkō Hyō (Ichimoku) ________________________________
     #
     # # Initialize Ichimoku Indicator
-    # indicator_ichimoku = ta.trend.IchimokuIndicator(high=df["<HIGH>"], low=df["<LOW>"], n1=10, n2=20, n3=30, visual=False, fillna=True)
+    # indicator_ichimoku = ta.trend.IchimokuIndicator(high=df["<HIGH>"],
+    #                                                 low=df["<LOW>"], n1=10, n2=20, n3=30, visual=False, fillna=True)
     #
     # # Add Ichimoku features
     # df['ichimoku_a'] = indicator_ichimoku.ichimoku_a()
@@ -295,7 +302,9 @@ def main():
     # # _____________________________ Parabolic Stop and Reverse (Parabolic SAR) ________________________________
     #     #
     #     # # Initialize PSAR Indicator
-    #     # indicator_psar = ta.trend.PSARIndicator(high=df["<HIGH>"], low=df["<LOW>"], close=df["<CLOSE>"], step=0.02, max_step=0.2)
+    #     # indicator_psar = ta.trend.PSARIndicator(high=df["<HIGH>"],
+    #                                               low=df["<LOW>"],
+    #                                               close=df["<CLOSE>"], step=0.02, max_step=0.2)
     #     #
     #     # # Add PSAR features
     #     # df['psar_i'] = indicator_psar.psar()
@@ -326,25 +335,320 @@ def main():
     # plt.title('Trix (TRIX)')
     # plt.legend()
     # plt.show()
+    #
+    # # _____________________________ Vortex Indicator (VI) ________________________________
+    #
+    # # Initialize VI Indicator
+    # indicator_vi = ta.trend.VortexIndicator(high=df["<HIGH>"],
+    #                                         low=df["<LOW>"],
+    #                                         close=df["<CLOSE>"], n=15, fillna=True)
+    #
+    # # Add VI features
+    # df['vi_diff'] = indicator_vi.vortex_indicator_diff()
+    # df['vi_neg'] = indicator_vi.vortex_indicator_neg()
+    # df['vi_pos'] = indicator_vi.vortex_indicator_pos()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['vi_diff'], label='Diff VI')
+    # plt.plot(df['vi_neg'], label='-VI')
+    # plt.plot(df['vi_pos'], label='+VI')
+    # plt.title('Vortex Indicator (VI)')
+    # plt.legend()
+    # plt.show()
 
-    # _____________________________ Vortex Indicator (VI) ________________________________
-
-    # Initialize VI Indicator
-    indicator_vi = ta.trend.VortexIndicator(high=df["<HIGH>"], low=df["<LOW>"], close=df["<CLOSE>"], n=15, fillna=True)
-
-    # Add VI features
-    df['vi_diff'] = indicator_vi.vortex_indicator_diff()
-    df['vi_neg'] = indicator_vi.vortex_indicator_neg()
-    df['vi_pos'] = indicator_vi.vortex_indicator_pos()
-
+    # # _____________________________________________________________________________________________________
+    # # _________________________________________ Others Indicators _________________________________________
+    # # _____________________________________________________________________________________________________
+    # # ______________________________________ Cumulative Return (CR) _______________________________________
+    #
+    # # Initialize CR Indicator
+    #
+    # indicator_cr = ta.others.CumulativeReturnIndicator(close=df["<CLOSE>"], fillna=True)
+    #
+    # # Add CR features
+    # df['cr_i'] = indicator_cr.cumulative_return()
+    #
+    #
+    # plt.plot(df['cr_i'], label='Cumulative Return (CR)')
+    # plt.title('Cumulative Return (CR)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ______________________________________ Daily Log Return (DLR) _______________________________________
+    # #
+    # # Initialize DLR Indicator
+    # indicator_dlr = ta.others.DailyLogReturnIndicator(close=df["<CLOSE>"], fillna=True)
+    #
+    # # Add DLR features
+    # df['dlr_i'] = indicator_dlr.daily_log_return()
+    #
+    #
+    # plt.plot(df['dlr_i'], label='Daily Return (DR)')
+    # plt.title('Daily Log Return (DLR)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # _____________________________________________________________________________________________________
+    # # _________________________________________ Volume Indicators _________________________________________
+    # # _____________________________________________________________________________________________________
+    # # ______________________________ Accumulation/Distribution Index (ADI) ________________________________
+    # #
+    # # Initialize ADI Indicator
+    # indicator_adi = ta.volume.AccDistIndexIndicator(high=df["<HIGH>"],
+    #                                                 low=df["<LOW>"],
+    #                                                 close=df["<CLOSE>"], volume=df["<VOL>"], fillna=True)
+    #
+    # # Add ADI features
+    # df['adi_i'] = indicator_adi.acc_dist_index()
+    #
+    #
+    # plt.plot(df['adi_i'], label='Accumulation/Distribution Index (ADI)')
+    # plt.title('Accumulation/Distribution Index (ADI)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ______________________________ Chaikin Money Flow (CMF) ________________________________
+    # #
+    # # Initialize CMF Indicator
+    #
+    # indicator_cmf = ta.volume.ChaikinMoneyFlowIndicator(high=df["<HIGH>"],
+    #                                                     low=df["<LOW>"],
+    #                                                     close=df["<CLOSE>"],
+    #                                                     volume=df["<VOL>"], n=20, fillna=True)
+    #
+    # # Add CMF features
+    # df['cmf_i'] = indicator_cmf.chaikin_money_flow()
+    #
+    #
+    # plt.plot(df['cmf_i'], label='CMF')
+    # plt.title('Chaikin Money Flow (CMF)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ______________________________ Ease of movement (EoM, EMV) ________________________________
+    # #
+    # # Initialize (EoM, EMV) Indicator
+    # indicator_cmf = ta.volume.EaseOfMovementIndicator(high=df["<HIGH>"],
+    #                                                   low=df["<LOW>"],
+    #                                                   volume=df["<VOL>"], n=20, fillna=True)
+    #
+    # # Add (EoM, EMV) features
+    # df['cmf_i'] = indicator_cmf.ease_of_movement()
+    # df['cmf_signal'] = indicator_cmf.sma_ease_of_movement()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['cmf_i'], label='Ease of movement (EoM, EMV)')
+    # plt.plot(df['cmf_signal'], label='Signal Ease of movement (EoM, EMV)')
+    # plt.title('Ease of movement (EoM, EMV)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ______________________________ Force Index (FI) ________________________________
+    # #
+    # # Initialize FI Indicator
+    # indicator_fi = ta.volume.ForceIndexIndicator(close=df["<CLOSE>"],
+    #                                              volume=df["<VOL>"], n=20, fillna=True)
+    #
+    # # Add FI features
+    # df['fi_i'] = indicator_fi.force_index()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['fi_i'], label='FI')
+    # plt.title('Force Index (FI)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ______________________________ Negative Volume Index (NVI) ________________________________
+    # #
+    # # Initialize NVI Indicator
+    # indicator_nvi = ta.volume.NegativeVolumeIndexIndicator(close=df["<CLOSE>"],
+    #                                                        volume=df["<VOL>"], fillna=True)
+    #
+    # # Add NVI features
+    # df['nvi_i'] = indicator_nvi.negative_volume_index()
+    #
     # plt.plot(df["<CLOSE>"])
-    plt.plot(df['vi_diff'], label='Diff VI')
-    plt.plot(df['vi_neg'], label='-VI')
-    plt.plot(df['vi_pos'], label='+VI')
-    plt.title('Vortex Indicator (VI)')
+    # plt.plot(df['nvi_i'], label='NVI')
+    # plt.title('Negative Volume Index (NVI)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ______________________________ On-balance volume (OBV) ________________________________
+    # #
+    # # Initialize OBV Indicator
+    # indicator_obv = ta.volume.OnBalanceVolumeIndicator(close=df["<CLOSE>"],
+    #                                                    volume=df["<VOL>"], fillna=True)
+    #
+    # # Add OBV features
+    # df['obv_i'] = indicator_obv.on_balance_volume()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['obv_i'], label='OBV')
+    # plt.title('On-balance volume (OBV)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ______________________________ Volume-price trend (VPT) ________________________________
+    # #
+    # # Initialize VPT Indicator
+    # indicator_vpt = ta.volume.VolumePriceTrendIndicator(close=df["<CLOSE>"],
+    #                                                     volume=df["<VOL>"], fillna=True)
+    #
+    # # Add VPT features
+    # df['vpt_i'] = indicator_vpt.volume_price_trend()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['vpt_i'], label='VPT')
+    # plt.title('Volume-price trend (VPT)')
+    # plt.legend()
+    # plt.show()
+
+    # # _____________________________________________________________________________________________________
+    # # ________________________________________ Momentum Indicators ________________________________________
+    # # _____________________________________________________________________________________________________
+    # # _________________________________________ Awesome Oscillator ________________________________________
+    # #
+    # # Initialize Awesome Oscillator Indicator
+    # indicator_ao = ta.momentum.AwesomeOscillatorIndicator(high=df["<HIGH>"],
+    #                                                       low=df["<LOW>"],
+    #                                                       s=5, len=34, fillna=True)
+    #
+    # # Add Awesome Oscillator features
+    # df['ao_i'] = indicator_ao.ao()
+    #
+    #
+    # plt.plot(df['ao_i'], label='AO')
+    # plt.title('Awesome Oscillator')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ________________________________ Kaufman’s Adaptive Moving Average (KAMA) __________________________________
+    # # #
+    # # Initialize KAMA Indicator
+    # indicator_kama = ta.momentum.KAMAIndicator(close=df["<CLOSE>"],
+    #                                            n=10,
+    #                                            pow1=2, pow2=30, fillna=True)
+    #
+    # # Add KAMA features
+    # df['kama_i'] = indicator_kama.kama()
+    #
+    # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['kama_i'], label='KAMA')
+    # plt.title('Kaufman’s Adaptive Moving Average (KAMA)')
+    # plt.legend()
+    # plt.show()
+    # # ________________________________ Money Flow Index (MFI) __________________________________
+    # #
+    # # Initialize MFI Indicator
+    # indicator_mfi = ta.momentum.MFIIndicator(high=df["<HIGH>"],
+    #                                          low=df["<LOW>"],
+    #                                          close=df["<CLOSE>"],
+    #                                          volume=df["<VOL>"],
+    #                                          n=14, fillna=True)
+    #
+    # # Add MFI features
+    # df['mfi_i'] = indicator_mfi.money_flow_index()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['mfi_i'], label='MFI')
+    # plt.title('Money Flow Index (MFI)')
+    # plt.legend()
+    # plt.show()
+    # # ________________________________ Rate of Change (ROC) __________________________________
+    # #
+    # # Initialize ROC Indicator
+    # indicator_roc = ta.momentum.ROCIndicator(close=df["<CLOSE>"],
+    #                                          n=12, fillna=True)
+    #
+    # # Add ROC features
+    # df['roc_i'] = indicator_roc.roc()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['roc_i'], label='ROC')
+    # plt.title('Rate of Change (ROC)')
+    # plt.legend()
+    # plt.show()
+    # # ________________________________ Relative Strength Index (RSI) __________________________________
+    # #
+    # # Initialize RSI Indicator
+    # indicator_rsi = ta.momentum.RSIIndicator(close=df["<CLOSE>"],
+    #                                          n=12, fillna=True)
+    #
+    # # Add RSI features
+    # df['rsi_i'] = indicator_rsi.rsi()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['rsi_i'], label='RSI')
+    # plt.title('Relative Strength Index (RSI)')
+    # plt.legend()
+    # plt.show()
+    # # # ________________________________ Stochastic Oscillator __________________________________
+    # # #
+    # # Initialize RSI Indicator
+    # indicator_stoch = ta.momentum.StochasticOscillator(high=df["<HIGH>"],
+    #                                                    low=df["<LOW>"],
+    #                                                    close=df["<CLOSE>"],
+    #                                                    n=14, fillna=True)
+    #
+    # # Add RSI features
+    # df['stoch_i'] = indicator_stoch.stoch()
+    # df['stoch_signal'] = indicator_stoch.stoch_signal()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['stoch_i'], label='Stochastic Oscillator')
+    # plt.plot(df['stoch_signal'], label='Signal Stochastic Oscillator')
+    # plt.title('Stochastic Oscillator')
+    # plt.legend()
+    # plt.show()
+    # # ________________________________ True strength index (TSI) __________________________________
+    # #
+    # # Initialize TSI Indicator
+    # indicator_tsi = ta.momentum.TSIIndicator(close=df["<CLOSE>"],
+    #                                          r=25, s=13, fillna=True)
+    #
+    # # Add TSI features
+    # df['tsi_i'] = indicator_tsi.tsi()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['tsi_i'], label='TSI')
+    # plt.title('True strength index (TSI)')
+    # plt.legend()
+    # plt.show()
+    #
+    # # # ________________________________ Ultimate Oscillator __________________________________
+    # # #
+    # # Initialize Ultimate Oscillator Indicator
+    # indicator_uo = ta.momentum.UltimateOscillator(high=df["<HIGH>"],
+    #                                               low=df["<LOW>"],
+    #                                               close=df["<CLOSE>"],
+    #                                               s=7,
+    #                                               m=14, len=28, ws=4.0, wm=2.0, wl=1.0, fillna=True)
+    #
+    # # Add Ultimate Oscillator features
+    # df['uo_i'] = indicator_uo.uo()
+    #
+    # # plt.plot(df["<CLOSE>"])
+    # plt.plot(df['uo_i'], label='UO')
+    # plt.title('Ultimate Oscillator')
+    # plt.legend()
+    # plt.show()
+
+    # # ________________________________ Williams %R __________________________________
+    # #
+    # Initialize Williams Indicator
+    indicator_wr = ta.momentum.WilliamsRIndicator(high=df["<HIGH>"],
+                                                  low=df["<LOW>"],
+                                                  close=df["<CLOSE>"],
+                                                  lbp=14, fillna=True)
+
+    # Add Williams features
+    df['wr_i'] = indicator_wr.wr()
+
+    plt.plot(df["<CLOSE>"])
+    plt.plot(df['wr_i'], label='Williams')
+    plt.title('Williams %R')
     plt.legend()
     plt.show()
-
 
     # print('*** Current Brent Oil price ***')
     # oil = exporter.lookup(name='Brent', market=Market.COMMODITIES,
